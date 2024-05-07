8b2b26905ccaf3c4d6fe775a2e603501217d423e
Tai <taiyebabatundejames@gmail.com>
2024-05-08 00:01:17 +0100

    deleted images

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     SECRET_KEY = '48a89ef0b9a63e63dc8a5ca21fc1871d'
     basedir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:08060110993@localhost/flask_eCommerce'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.abspath('static/profile_pic')
     DEBUG = True
