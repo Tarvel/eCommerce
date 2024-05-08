d5f87a45902420664917ff5e792cd4e591129ff3
Tai <taiyebabatundejames@gmail.com>
2024-05-08 01:36:39 +0100

    update

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     SECRET_KEY = '48a89ef0b9a63e63dc8a5ca21fc1871d'
     basedir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'postgresql://flask_ecommerce_81dp_user:KD5OFXHSHIOdhpmoZ48j8zC1B2gyvX1J@dpg-cotb936g1b2c73de8qeg-a.oregon-postgres.render.com/flask_ecommerce_81dp'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://flask_ecommerce_81dp_user:KD5OFXHSHIOdhpmoZ48j8zC1B2gyvX1J@dpg-cotb936g1b2c73de8qeg-a/flask_ecommerce_81dp'
     # postgresql://postgres:08060110993@localhost/flask_eCommerce
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.abspath('static/profile_pic')
