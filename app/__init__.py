347cd828f8629f3ffdb30048facaef932d21a31a
Tai <taiyebabatundejames@gmail.com>
2024-08-14 13:42:55 +0100

    update

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
+from app.forms import  DeleteForm
+
 db = SQLAlchemy()
 migrate = Migrate()
 login_manager = LoginManager()
@@ -23,6 +25,47 @@ def create_app(config_class=DevelopmentConfig):
     from .routes import auth, product
     app.register_blueprint(auth)
     app.register_blueprint(product)
+
+
+    from flask import Flask
+from config import DevelopmentConfig
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+migrate = Migrate()
+login_manager = LoginManager()
+bcrypt = Bcrypt()
+
+def create_app(config_class=DevelopmentConfig):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    migrate.init_app(app, db)
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
+
+    from .routes import auth, product
+    app.register_blueprint(auth)
+    app.register_blueprint(product)
+
+    @app.context_processor
+    def inject_delete_form():
+        delete = DeleteForm()
+        return dict(delete=delete)
+
+    @app.cli.command('create_db')
+    def create_db():
+        """Create the database tables."""
+        db.create_all()
+        print("Database tables created.")
+
+    return app
+
 
     @app.cli.command('create_db')
     def create_db():
